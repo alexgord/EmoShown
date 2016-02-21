62ba592aa0ea62215b888915615501037262727f
Alexandre Simard <alesimar@gmail.com>
2016-02-21 02:10:04 -0500

    Added random message and added some styling

--- EmoShown/routes/index.js
+++ EmoShown/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var sentiment = require('sentiment');
 var request = require('request');
 var striptags = require('striptags');
-var afinn = require('../AFINN.json');
+var lib = require('../lib.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,30 +24,43 @@ router.post('/result', function(req, res)
 {
     var text = req.body.sample;
     
+    var positiveResponses = ["This page makes duck happy!",
+        "Better than bread!", "Good read!", "Quacking good!"];
+        
+    var negativeResponses = ["This page made duck sad", "I'd rather have bread", "A bunch of quack", "This webpage made me want to duck away"];
+    
+    var neutralResponses = ["Duck doesn't know what to think of this webpage", "I am a duck, I cannot read", "Quack", "Mostly harmless to ducks"];
+    
+    var response = "";
+    
     request({uri: text}, function(err, response, body)
     {
         var sent = sentiment(striptags(body));
         var picture = "";
         if(Math.abs(sent.comparative) < 0.01)
         {
-            picture = "images/neutral.png"
+            picture = "images/neutral.png";
+            response = neutralResponses[lib.getRandomInt(0, neutralResponses.length)];
         }
         else
         {
             if(sent.comparative > 0)
             {
                 picture = "images/happy.png";
+                response = positiveResponses[lib.getRandomInt(0, positiveResponses.length)];
             }
             else
             {
                 picture = "images/sad.png";
+                response = negativeResponses[lib.getRandomInt(0, negativeResponses.length)];
             }
         }
         
         var value = (sent.comparative + 5) * 10;
         
-        res.render('result', { title: 'Happiness',
-            picture:picture, value: value.toFixed(2)});
+        res.render('result', { title: 'Score',
+            picture:picture, value: value.toFixed(2),
+            responsetext: response});
     });
 });
 
